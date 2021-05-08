f7ca3fc47f807c3f5fd409dfd418732de6b83d82
Nontasit <p.nontasit@gmail.com>
2021-05-08 13:16:45 +0700

    import backup file from my computer to work directory wait to fix style in css

---
+++ elements/contact-footer.php
@@ -0,0 +1,54 @@
+<!-- Contact-tagline -->
+<section id="contact-tagline" class="section">
+    <div class="container">
+        <div class="columns is-vcentered">
+            <div class="column">
+                <p class="is-size-1 has-text-weight-bold has-text-centered">ติดต่อเราเลยวันนี้!</p>
+            </div>
+            <div class="column">
+                <a href="https://lin.ee/96so2yC"><img src="imgs/contact-tagline-img/line@-img.png" alt="ID Line: @starpetsbydeedog" title="ID Line: @starpetsbydeedog"></a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer Section -->
+<footer>
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <nav>
+                    <a href="index.php">หน้าแรก</a>
+                    <a href="#">เกี่ยวกับเรา</a>
+                    <a href="#">สินค้าของเรา</a>
+                    <a href="data/promotions/promotions-landing-pg.php">โปรโมชั่น</a>
+                    <a href="data/articles/articles-landing-pg.php">บทความน่าสนใจ</a>
+                    <a href="q-and-a.php">คำถามที่พบบ่อย</a>
+                    <a href="starpetlife.php">สตาเพ็ทไลฟ์</a>
+                </nav>
+            </div>
+            <div class="column">
+                <p>ติดตามเราได้ที่</p>
+                <div class="social">
+                    <div class="social-group">
+                        <img src="imgs/social-img/fb.svg" alt="FB: starpetsbydeedog" title="FB: starpetsbydeedog">
+                        <p>@starpetsbydeedog</p>
+                    </div>
+                    <div class="social-group">
+                        <img src="imgs/social-img/line.svg" alt="Line: @starpetsbydeedog" title="Line: @starpetsbydeedog">
+                        <p><a href="https://lin.ee/96so2yC">@starpetsbydeedog</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <p>บริษัท สตาร์ออลเพ็ทส์ จำกัด สำนักงานใหญ่</p>
+                <p>49/62 หมู่ที่ 2 ถนน แจ้งวัฒนะ ต.บางตลาด อ.ปากเกร็ด จ.นนทบุรี 11120</p>
+                <p>02-xxx-xxx</p>
+                <p>สาขา เซ็นทรัลพลาซ่า แจ้งวัฒนะ</p>
+                <p>02-835-3711</p>
+                <p>สาขา เซ็นทรัลพลาซ่า เวสต์เกต</p>
+                <p>02-102-7729</p>
+            </div>
+        </div>
+        <p class="copyright"><span>&copy;</span>2021 Starpets by Dee Dog</p>
+    </div>
+</footer>
