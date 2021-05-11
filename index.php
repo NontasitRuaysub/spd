cc57bbeb2cb1e2f159b068b250c16c41c13d0567
Nontasit <p.nontasit@gmail.com>
2021-05-11 15:16:42 +0700

    spd with navbar without search function

--- index.php
+++ index.php
@@ -28,7 +28,83 @@
         <div id="shop-announce" class="F26938">
             <p>ประกาศจากทางร้าน</p>
         </div>
+
         <!-- Navbar -->
+        <nav id="header-navbar" class="FFF8EE">
+            <!-- navbar logo image -->
+            <div class="navbar-cs-logo">
+                <a href="index.php">
+                    <img src="imgs/logo-img/logo.png" alt="STAR PETS by DEE DOG Logo" title="STAR PETS by DEE DOG">
+                </a>
+            </div>
+            <div class="navbar-cs-menu">
+                <ul>
+                    <li><a href="#">หน้าแรก</a></li>
+                    <li class="has-dropdown"><a href="#">สินค้าของเรา<span class="arrow pd-arrow">&#62;</span></a>
+                        <div class="first-dropdown FFF8EE">
+                            <ul>
+                                <li class="product"><a href="#">สินค้าน้องหมา<span class="arrow">&#62;</span></a>
+                                    <div class="second-dropdown FFF8EE">
+                                        <ul>
+                                            <li class="food-product"><a href="#">อาหาร<span class="arrow">&#62;</span></a>
+                                                <div class="third-dropdown FFF8EE">
+                                                    <ul>
+                                                        <li><a href="#">อาหารเปียก</a></li>
+                                                        <li><a href="#">อาหารแห้ง</a></li>
+                                                    </ul>
+                                                </div>
+                                        </li>
+                                            <li><a href="#">ขนม</a></li>
+                                            <li><a href="#">ของใช้</a></li>
+                                            <li><a href="#">ของเล่น</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="product"><a href="#">สินค้าน้องแมว<span class="arrow">&#62;</span></a>
+                                    <div class="second-dropdown FFF8EE">
+                                        <ul>
+                                            <li class="food-product"><a href="#">อาหาร<span class="arrow">&#62;</span></a>
+                                                <div class="third-dropdown FFF8EE">
+                                                    <ul>
+                                                        <li><a href="#">อาหารเปียก</a></li>
+                                                        <li><a href="#">อาหารแห้ง</a></li>
+                                                    </ul>
+                                                </div>
+                                        </li>
+                                            <li><a href="#">ขนม</a></li>
+                                            <li><a href="#">ของใช้</a></li>
+                                            <li><a href="#">ของเล่น</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="product"><a href="#">สินค้าสัตว์อื่นๆ<span class="arrow">&#62;</span></a>
+                                    <div class="second-dropdown FFF8EE">
+                                        <ul>
+                                            <li class="food-product"><a href="#">อาหาร<span class="arrow">&#62;</span></a>
+                                                <div class="third-dropdown FFF8EE">
+                                                    <ul>
+                                                        <li><a href="#">อาหารเปียก</a></li>
+                                                        <li><a href="#">อาหารแห้ง</a></li>
+                                                    </ul>
+                                                </div>
+                                        </li>
+                                            <li><a href="#">ขนม</a></li>
+                                            <li><a href="#">ของใช้</a></li>
+                                            <li><a href="#">ของเล่น</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li><a href="#">คำถามที่พบบ่อย</a></li>
+                    <li><a href="#">บทความ</a></li>
+                    <li><a href="#">สาขาของเรา</a></li>
+                </ul>
+            </div>
+            <!-- Nav Burger -->
+            <i class="fas fa-bars fa-2x burger"></i>
+        </nav>
     </header>
     <main>
         <!-- Carousel Section -->
